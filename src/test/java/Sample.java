a71308178798c061eff371dd983ed3a1194520d6
gowthamautomation <gowthammech444@gmail.com>
2024-09-27 19:03:23 +0530

    jira

---
+++ src/test/java/Sample.java
@@ -0,0 +1,51 @@
+
+public class Sample {
+	public static void main(String[] args) {
+
+		int days=5;
+		
+		switch (days) {
+		case 1:
+			System.out.println("monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+			break;
+
+		case 3:
+			System.out.println("Wednesday");
+			break;
+
+		case 4:
+			System.out.println("Thursday");
+			break;
+		case 5:
+			System.out.println("Friday");
+			break;
+
+		case 6:
+			System.out.println("Saturday");
+			break;
+
+		case 7:
+			System.out.println("Sunday");
+			break;
+
+			default:
+			System.out.println("enter the number");
+			break;
+
+		
+		
+		
+		
+		}
+		
+		
+		
+		
+		
+
+	}
+
+}
